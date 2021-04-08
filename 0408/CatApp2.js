d314a923ce9644b9098eb940d0c61bb6e16f9ba3
shootersinon <oliver800999@gmail.com>
2021-04-08 12:34:50 +0800

    CAT2.js

---
+++ 0408/CatApp2.js
@@ -0,0 +1,56 @@
+import { render } from "@testing-library/react";
+import React from "react";
+
+function formatDate(date) {
+  return date.toLocaleDateString();
+}
+
+class Avatar extends React.Component {
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return (
+      <img
+        className="Avatar"
+        src={this.props.user.avatarUrl}
+        alt={this.props.user.name}
+      />
+    );
+  }
+}
+
+class UserInfo extends React.Component {
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return (
+      <div className="UserInfo">
+        <Avatar user={this.props.user} />
+        <div className="UserInfo-name">{this.props.user.name}</div>
+      </div>
+    );
+  }
+  
+}
+
+class Comment extends React.Component {
+  constructor(props){
+    super(props);
+  }
+  render(){
+  return (
+    <div className="Comment">
+      <UserInfo user={this.props.author} />
+      <div className="Comment-text">{this.props.text}</div>
+      <div className="Comment-date">
+        {formatDate(this.props.date)}
+      </div>
+    </div>
+  );
+  }
+}
+ 
+ 
+ export default Comment;
