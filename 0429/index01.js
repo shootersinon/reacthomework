90fcd66625b37b87fdace1453b55806f8f5b7426
shootersinon <oliver800999@gmail.com>
2021-05-06 11:24:31 +0800

    index01.js

---
+++ 0429/index01.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+import ReactDOM from 'react-dom';
+
+import Greeting from './App01.js';
+
+
+ReactDOM.render(<Greeting isLoggedIn={true} />, document.getElementById('root'));
